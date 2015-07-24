2b85e39cd856ad4a530a03923e8cd46f5eee7acf
Conan Tzou <conantzou@gmail.com>
2015-07-24 12:03:20 -0700

    add more components to comment box

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -1,19 +1,44 @@
+// components
 var CommentBox = React.createClass({
   render: function() {
     return (
-      // <div className="commentBox">
-      //   I am a CommentBox.
-      // </div>
-      React.createElement(
-        "div",
-        { className: "commentBox" },
-        "Hello I'm a CommentBox"
-      )
+      // JSX
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+
+      // same as:
+      // React.createElement(
+      //   "div",
+      //   { className: "commentBox" },
+      //   "Hello I'm a CommentBox"
+      // )
     );
   }
 });
 
+// more components
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="CommentList">
+        I am a CommentList
+      </div>
+    );
+  }
+});
 
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        I am a CommentForm
+      </div>
+    );
+  }
+});
 
 React.render(
   <CommentBox />,
