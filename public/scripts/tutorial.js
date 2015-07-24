3b142597bbc7dc7d36cf673b1f12cd2e5f9ef5e2
Conan Tzou <conantzou@gmail.com>
2015-07-24 12:23:29 -0700

    hook up data modularly and render comments dynamically

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -1,3 +1,8 @@
+var data = [
+  {author: "conan", text: "this is one comment"},
+  {author: "annie", text: "this is *another* comment"}
+];
+
 // components
 var CommentBox = React.createClass({
   render: function() {
@@ -5,7 +10,7 @@ var CommentBox = React.createClass({
       // JSX
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
         <CommentForm />
       </div>
 
@@ -22,10 +27,17 @@ var CommentBox = React.createClass({
 // more components
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
+
     return (
       <div className="CommentList">
-        <Comment author="conan">This is one comment</Comment>
-        <Comment author="annie">this is *another* comment</Comment>
+        {commentNodes}
       </div>
     );
   }
@@ -57,6 +69,6 @@ var Comment = React.createClass({
 })
 
 React.render(
-  <CommentBox />,
+  <CommentBox data={data} />,
   document.getElementById("content")
 );
