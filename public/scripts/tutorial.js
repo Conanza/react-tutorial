577792c9d069dff48db90bec50ff9532be4354d7
Conan Tzou <conantzou@gmail.com>
2015-07-24 13:23:03 -0700

    setup events and handlers to handle form submission, posting data to the server, and refreshing the list

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -16,6 +16,21 @@ var CommentBox = React.createClass({
     });
   },
 
+  handleCommentSubmit: function(comment) {
+    $.ajax({
+      url: this.props.url,
+      dataType: "json",
+      type: "POST",
+      data: comment,
+      success: function (data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
   getInitialState: function() {
     return {data: []};
   },
@@ -29,9 +44,10 @@ var CommentBox = React.createClass({
     return (
       // JSX
       <div className="commentBox">
+        <h1>Post Comment</h1>
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
         <h1>Comments</h1>
         <CommentList data={this.state.data} />
-        <CommentForm />
       </div>
 
       // same as:
@@ -73,7 +89,7 @@ var CommentForm = React.createClass({
       return;
     }
 
-    // TODO: send request to the server
+    this.props.onCommentSubmit({author: author, text: text});
     React.findDOMNode(this.refs.author).value = "";
     React.findDOMNode(this.refs.text).value = "";
     return;
